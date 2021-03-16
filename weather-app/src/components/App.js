66f8f6f7f2a70e42420bcd7ee66d71b450fd92cb
Shira-Meirovitz <shirameirovi@gmail.com>
2021-03-16 15:59:44 +0200

    added css

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
-import DayWeather from "./components/DayWeather";
+import "./DayWeather.css";
+import DayWeather from "./DayWeather";
 
 class Day {
   constructor(day, image, minTemperature, maxTemperature) {
