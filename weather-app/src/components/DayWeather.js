25c3eec935a77121220d6948bc5f3d1022c1aa59
Shira-Meirovitz <shirameirovi@gmail.com>
2021-03-16 15:43:29 +0200

    created dayweather class

---
+++ weather-app/src/components/DayWeather.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class DayWeather extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="day-weather">
+        <p>{this.props.day}</p>
+        <img className="image" alt="" src={this.props.image}></img>
+        <div className="temperature">
+          <span className="minTemperature">{this.props.minTemperature}º</span>
+          <span className="maxTemperature">{this.props.maxTemperature}º</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+DayWeather.defaultProps = {
+  day: "happy",
+  image: "./cloudy.png",
+  minTemperature: "20",
+  maxTemperature: "38",
+};
